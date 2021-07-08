5cb8a115643a8022dfd0d4d959d77ca8132f8796
Intyaud Banton <65741007+ibvisuals@users.noreply.github.com>
2021-07-08 02:22:50 -0500

    Update

--- app/src/main/java/com/example/roadtrip/MainActivity.java
+++ app/src/main/java/com/example/roadtrip/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.roadtrip;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_graph);
+        NavController navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
-    public void signIn(View view) {
-    }
+
 }
---
+++ app/src/main/java/com/example/roadtrip/SignInFragment.java
@@ -0,0 +1,109 @@
+package com.example.roadtrip;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class SignInFragment extends Fragment {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private EditText loginEmail;
+    private EditText loginPassword;
+    private Button signInBtn;
+
+
+
+    public SignInFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loginEmail = view.findViewById(R.id.loginEmail);
+        loginPassword = view.findViewById(R.id.loginPassword);
+        signInBtn = view.findViewById(R.id.signInBtn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickSignIn();
+            }
+        });
+    }
+
+    private void onClickSignIn() {
+
+        if (TextUtils.isEmpty(loginEmail.getText())) {
+            Toast.makeText(requireContext(), "Please enter email.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(loginPassword.getText())) {
+            Toast.makeText(requireContext(), "Please enter password.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String emailContent = loginEmail.getText().toString();
+        String passwordContent = loginPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(emailContent, passwordContent).addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success");
+                    updateUI();
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.getException());
+                    Toast.makeText(requireContext(), "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private void updateUI() {
+
+    }
+}
